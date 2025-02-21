edd39e39dd688814d47114d5b88d67874a402dbe
kashif2666 <kashifjsr7@gmail.com>
2025-02-21 18:27:12 +0530

    api endpoints for movies

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 
 import authRoutes from "./routes/auth.route.js";
-import { ENV_VARS } from "./config/envVars.js";
+import movieRoutes from "./routes/movie.route.js";
+
 import { connectDB } from "./config/db.js";
+import { ENV_VARS } from "./config/envVars.js";
 
 const app = express();
 
@@ -11,6 +13,7 @@ const PORT = ENV_VARS.PORT;
 app.use(express.json()); // will allow us to parse body
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT: ${PORT}`);

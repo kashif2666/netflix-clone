6edbfde0f4c3661be53fb248332e831a75b587bc
kashif2666 <kashifjsr7@gmail.com>
2025-02-21 20:07:18 +0530

    api endpoint for tv shows

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
 
 import { connectDB } from "./config/db.js";
 import { ENV_VARS } from "./config/envVars.js";
@@ -14,6 +15,7 @@ app.use(express.json()); // will allow us to parse body
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", movieRoutes);
+app.use("/api/v1/tv", tvRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT: ${PORT}`);

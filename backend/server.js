1c468daea2170865fc58d03f1a371f381e0e0326
kashif2666 <kashifjsr7@gmail.com>
2025-02-21 21:34:39 +0530

    protectRoute middleware added

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,19 @@ import tvRoutes from "./routes/tv.route.js";
 
 import { connectDB } from "./config/db.js";
 import { ENV_VARS } from "./config/envVars.js";
+import { protectRoute } from "./middleware/protectRoute.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); // will allow us to parse body
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT: ${PORT}`);

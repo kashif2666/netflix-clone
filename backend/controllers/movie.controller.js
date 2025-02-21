c6b09e2c8f1627d75be176b5c2135ab34f23f475
kashif2666 <kashifjsr7@gmail.com>
2025-02-21 19:23:20 +0530

    moving services folder

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -1,4 +1,4 @@
-import { fetchFromTMDB } from "../../services/tmdb.service.js";
+import { fetchFromTMDB } from "../services/tmdb.service.js";
 
 export async function getTrendingMovie(req, res) {
   try {

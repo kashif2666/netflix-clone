d8d1ac165274c8006f6ac71f829f8e9c756ba7a6
kashif2666 <kashifjsr7@gmail.com>
2025-02-23 14:13:16 +0530

    Auth Screen UI Design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/home/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,4 +1,8 @@
+import AuthScreen from "./AuthScreen";
+import HomeScreen from "./HomeScreen";
+
 const HomePage = () => {
-  return <div className="hero-bg h-screen">HomePage</div>;
+  const user = false;
+  return <div>{user ? <HomeScreen /> : <AuthScreen />}</div>;
 };
 export default HomePage;
